a5cb03aafb6b45d78a1e78dcf85e7d6bfdbf0370
Jason Morawski <jmorawsk@ualberta.ca>
2012-11-26 07:56:54 -0700

    Updated World tests

--- v3FantasyEmpires/src/tests/world/WorldTests.java
+++ v3FantasyEmpires/src/tests/world/WorldTests.java
@@ -2,8 +2,13 @@ package tests.world;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import code.Civilization;
 import code.World;
 
 
@@ -20,7 +25,7 @@ public class WorldTests
         assertNotNull(w.getDensity());
         assertNotNull(w.getSeason());
         assertNotNull(w.getSize());
-        
+
     }
     @Test
     public void testParameterConstructor()
@@ -65,21 +70,78 @@ public class WorldTests
         }
     }
     @Test
-    public void testCivList(){
-        //TODO
+    public void testAddCiv(){
         World w = new World();
+        Civilization civ = new Civilization(null,"","",1);
+        Civilization civ2 = new Civilization(null,"","",1);
+        w.addCiv(civ);
+        w.addCiv(civ2);
+        ArrayList<Civilization> civs = w.getCivs();
+        assertEquals(civ,civs.get(0));
+        assertEquals(civ2,civs.get(1));
+    }
+    @Test
+    public void testCivList(){
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        World w = new World();
+        int num = 5;
+        spawnTestCivs(w,num);
+        w.CivList();
+
+        String result = outContent.toString();
+        String expected;
+        for (int n=0;n<num;n++){
+            expected = "TestCiv" + (n+1);
+            assertTrue(result.indexOf(expected)!=-1);
+        }
     }
 
     @Test
-    public void testSetPlayer(){
-        //TODO
+    public void testSetPlayerValid(){
+        int num=5;
+        int player=1;
         World w = new World();
-        
+        spawnTestCivs(w, num);
+        w.setPlayer(player);
+        for (int n=0;n<num;n++){
+            if (n!=player-1)
+                assertFalse(w.getCivs().get(n).isPlayer());
+            else
+                assertTrue(w.getCivs().get(n).isPlayer());
+        }
     }
-    public void test()
-    {
-
-        fail("Not yet implemented");
+    @Test
+    public void testSetPlayerTooLarge(){
+        int num=5;
+        int player=6;
+        World w = new World();
+        spawnTestCivs(w, num);
+        w.setPlayer(player);
+        for (int n=0;n<num;n++){
+            assertFalse(w.getCivs().get(n).isPlayer());
+        }
+    }
+    @Test
+    public void testSetPlayerTooSmall(){
+        int num=5;
+        int player=0;
+        World w = new World();
+        spawnTestCivs(w, num);
+        w.setPlayer(player);
+        for (int n=0;n<num;n++){
+            assertFalse(w.getCivs().get(n).isPlayer());
+        }
+    }
+    public void spawnTestCivs(World w,int num){
+        Civilization civ;
+        for (int n=0;n<num;n++){
+            civ = new Civilization(null,"","",1);
+            civ.setName("TestCiv" + (n+1));
+            w.addCiv(civ);
+        }
     }
 
 }
